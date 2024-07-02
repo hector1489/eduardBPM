97a4c68e7899cad1d501aa2d3286bbe2d683e8cd
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-07-02 13:41:27 -0400

    commit logic %

--- buttonsQuestions.js
+++ buttonsQuestions.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function showQuestion(index) {
     const questions = getQuestions();
-    questions.forEach((question, i) => {
+    questions?.forEach((question, i) => {
       question.classList.toggle('active', i === index);
     });
   }
@@ -30,15 +30,40 @@ document.addEventListener('DOMContentLoaded', () => {
   function addChangeListenerToQuestions() {
     const activeModule = document.querySelector('.module-section.active');
     const questions = activeModule.querySelectorAll('.pregunta select');
-    questions.forEach(select => {
+    questions?.forEach(select => {
       select.addEventListener('change', nextQuestion);
     });
   }
 
+  /*function countQuestionsInModules() {
+    const modules = document.querySelectorAll('.module-section');
+    modules.forEach(module => {
+      const questions = module.querySelectorAll('.pregunta');
+      const totalQuestions = questions.length;
+      let answeredQuestions = 0;
+      let totalPercentage = 0;
 
+      questions.forEach(question => {
+        const select = question.querySelector('select');
+        const input = question.querySelector('input');
+        //suma valores seleccionados
+        if (select && select.value) {
+          totalPercentage += parseInt(select.value, 10);
+          answeredQuestions++;
+        } else if (input && input.value) {
+          answeredQuestions++;
+        }
+      });
+
+       //promedio de los porcentajes por consola
+      const averagePercentage = totalPercentage / totalQuestions;
+      console.log(`Module ${module.id} has ${totalQuestions} questions, ${answeredQuestions} answered. Average response percentage: ${averagePercentage.toFixed(2)}%.`);
+    });
+  }*/
 
   showQuestion(currentQuestion);
   addChangeListenerToQuestions();
+  //countQuestionsInModules();
 
   window.nextQuestion = nextQuestion;
   window.previousQuestion = previousQuestion;
---
+++ questions.md
@@ -0,0 +1,28 @@
+Module module-ficha has 0 questions.
+Module module-infraestructura has 3 questions.
+Module module-legales has 4 questions.
+Module module-poes-control-productos has 3 questions.
+Module module-poes-control-agua has 0 questions.
+Module module-poes-superficies has 9 questions.
+Module module-poes-contaminacion-cruzada has 2 questions.
+Module module-poes-sustancias-adulterantes has 2 questions.
+Module module-poes-higiene-empleados has 5 questions.
+Module module-poes-control-plagas has 2 questions.
+Module module-poes-instalaciones has 2 questions.
+Module module-poe-recepcion has 3 questions.
+Module module-poe-almacenamiento has 8 questions.
+Module module-poe-preelaboraciones has 5 questions.
+Module module-poe-elaboracion has 10 questions.
+Module module-poe-mantencion has 0 questions.
+Module module-poe-transporte has 2 questions.
+Module module-poe-servicio has 5 questions.
+Module module-poe-lavado-ollas-vajilla has 3 questions.
+Module module-poe-control-calidad has 4 questions.
+Module module-ma has 7 questions.
+Module module-doc has 6 questions.
+Module module-tra has 11 questions.
+Module module-lum has 0 questions.
+Module module-resumen has 0 questions.
+
+
+
